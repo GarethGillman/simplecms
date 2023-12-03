ea3aa4609f634f867784ba9f29fca691b0ee54f8
Gareth Gillman <gillman56@gmail.com>
2023-12-03 18:07:24 +0000

    error page changes

--- resources/views/dashboard/errors/404.blade.php
+++ resources/views/dashboard/errors/404.blade.php
@@ -6,8 +6,13 @@
     <div class="container">
 
         <h1>{{ ('Error 404') }}</h1>
-        <p>This page doesn't exist, click the link below to go to the dashboard.</p>
-        <a class="button" href="{{ route('dashboard') }}">Dashboard</a>
+        <p>This page doesn't exist, click the link below to go to a relevant place.</p>
+
+        @if( str_contains( url()->current(), '/dashboard' ) )
+            <a class="button" href="{{ route('dashboard') }}">Dashboard</a>
+        @else
+            <a class="button" href="{{ url('/') }}">Home</a>
+        @endif
         
     </div>
 </section>
