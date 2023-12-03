5283f5199d03d1c56ffa597c96467e71fff0f709
Gareth Gillman <gillman56@gmail.com>
2023-12-03 17:00:35 +0000

    add settings table, controller and model

---
+++ database/seeders/SettingsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('settings')->insert([
+            'website_name' => 'A SimpleCMS Website',
+            'setup' => 'false',
+        ]);
+    }
+}
